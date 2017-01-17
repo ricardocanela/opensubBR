42510e39dc3e1e9a8f92ac76e770f594e5e09609
Tulio Coelho <tulioopc@gmail.com>
2017-01-17 20:11:59 -0200

    Configuring opensubtitles-universal-api

--- src/components/helloworld.js
+++ src/components/helloworld.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Hello = () => {
   return (
-    <h1> Hello World</h1>
+    <h1>OpenSubs</h1>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import OpenSub from 'opensubtitles-universal-api';
 import Hello from './components/helloworld.js'
 
+const OpenSubtitles = new OpenSub('tuliocoelho');
+
+const episodeQuery = {
+  imdbid: 'tt0411008', // lost-2004 abc
+  season: 1,
+  episode: 1
+};
+
+OpenSubtitles.search(episodeQuery).then(result => {
+  console.log(result.en[0]);
+});
 
 
 const App = ()=>{
